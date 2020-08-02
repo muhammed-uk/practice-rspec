97f4ecede1ab486361f59b2159fadf6a2a73cc6a
Muhammed <muhammed.uk@kreatio.com>
2020-08-02 17:30:21 +0530

    Muhammed | adding rank and suits to cards, some refactoring

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,25 @@
 class Card
-  attr_reader :type
+  attr_reader :rank, :suit
 
-  def initialize(type)
-    @type = type
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
   end
 end
 
-RSpec.describe 'Card' do
-  it 'has a type' do
-    card_type = 'Ace of Spades'
-    card = Card.new(card_type)
-    expect(card.type).to eq(card_type)
+RSpec.describe Card do
+
+  before(:all) do
+    @rank = 'Ace'
+    @suit = 'Spades'
+    @card = Card.new(@rank, @suit)
+  end
+
+  it 'has a rank' do
+    expect(@card.rank).to eq(@rank)
+  end
+
+  it 'has a suit' do
+    expect(@card.suit).to eq(@suit)
   end
 end
